d6fb75875a9fdaa70ad2f00f7f55d9c4cb4f0d31
Sahrija <114372392+Sahrija@users.noreply.github.com>
2023-01-22 11:34:31 +0700

    add berita.html

--- js/script.js
+++ js/script.js
@@ -1,16 +1,46 @@
 
-function regist(){
+function regist() {
+
+
+    window.localStorage("")
+}
+
+// var navbar = document.querySelector('#login')
+
+// window.onscroll = function() {
+
+//   // pageYOffset or scrollY
+//   if (window.pageYOffset > 421.3333435058594) {
+//     navbar.classList.add('scrolled')
+//   } else {
+//     navbar.classList.remove('scrolled')
+//   }
+// }
+
+
+var url = window.location.href;
+function ulrExist(url){
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    if (http.status != 404)
+        return true;
     
+    else
+        return false;
 }
 
-var navbar = document.querySelector('#login')
+console.log(ulrExist());
+console.log("AAAAAA")
 
-window.onscroll = function() {
 
-  // pageYOffset or scrollY
-  if (window.pageYOffset > 421.3333435058594) {
-    navbar.classList.add('scrolled')
-  } else {
-    navbar.classList.remove('scrolled')
-  }
-}
+// function hasLogIn(){
+//     if (window.localStorage.getItem("currentUser") in ["", null, undefined]){
+//         return false;
+//     }
+//     else{return true}
+// }
+// if(hasLogIn()){
+//     accountName = window.localStorage.getItem("currentUser");
+//     accountLabel = document.getElementById("account").innerHTML = accountName
+// }
