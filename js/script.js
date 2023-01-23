f74447ce7ba83882ed8807a80c28e26858aa5532
Sahrija <114372392+Sahrija@users.noreply.github.com>
2023-01-23 11:43:57 +0700

    patch 1.2.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+// // darkmode toggle
+// let darkModeState = false;
+
+// const button = document.querySelector("#dark-mode-toggle");
+
+// // MediaQueryList object
+// const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+// // Toggles the "dark-mode" class
+// function toggleDarkMode(state) {
+//     document.documentElement.classList.toggle("dark-mode", state);
+//     darkModeState = state;
+// }
+
+// // Sets localStorage state
+// function setDarkModeLocalStorage(state) {
+//     localStorage.setItem("dark-mode", state);
+// }
+
+// // Initial setting
+// toggleDarkMode(localStorage.getItem("dark-mode") == "true");
+
+// // Listen for changes in the OS settings.
+// // Note: the arrow function shorthand works only in modern browsers,
+// // for older browsers define the function using the function keyword.
+// useDark.addListener((evt) => toggleDarkMode(evt.matches));
+
+// // Toggles the "dark-mode" class on click and sets localStorage state
+// button.addEventListener("click", () => {
+//     darkModeState = !darkModeState;
+
+//     toggleDarkMode(darkModeState);
+//     setDarkModeLocalStorage(darkModeState);
+// });
 
 function regist() {
 
@@ -19,13 +53,13 @@ function regist() {
 
 
 var url = window.location.href;
-function ulrExist(url){
+function ulrExist(url) {
     var http = new XMLHttpRequest();
     http.open('HEAD', url, false);
     http.send();
     if (http.status != 404)
         return true;
-    
+
     else
         return false;
 }
