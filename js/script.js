a81a92680cc4f0d66eec2505790e67ce9c7daa77
Sahrija <114372392+Sahrija@users.noreply.github.com>
2023-01-24 09:02:24 +0700

    dark mode fixed & about page etc.

--- js/script.js
+++ js/script.js
@@ -1,83 +1,88 @@
 
 function regist() {
+  let password = document.getElementById("password").value;
+  let checkPassword = document.getElementById("check-password").value;
 
+  event.preventDefault()
 
-    window.localStorage("")
+  if (password == checkPassword) {
+    username = document.getElementById("username").value;
+    
+    window.localStorage.setItem("currentUser", username);
+    alert("Anda berhasil membuat akun");
+    window.location = "home.html";
+  }else{
+    alert("Cek password dan password yang anda masukkan tidak sesuai");
+  }
 }
 
-// var navbar = document.querySelector('#login')
-
-// window.onscroll = function() {
-
-//   // pageYOffset or scrollY
-//   if (window.pageYOffset > 421.3333435058594) {
-//     navbar.classList.add('scrolled')
-//   } else {
-//     navbar.classList.remove('scrolled')
-//   }
-// }
 
 
-var url = window.location.href;
-function ulrExist(url) {
-    var http = new XMLHttpRequest();
-    http.open('HEAD', url, false);
-    http.send();
-    if (http.status != 404)
-        return true;
+// darkmode for all documents
+let darkModeState = false;
 
-    else
-        return false;
+const button = document.querySelector("#dark-mode-toggle");
+
+// MediaQueryList object
+const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+
+// Sets localStorage state
+function setDarkModeLocalStorage(state) {
+  localStorage.setItem("dark-mode", state);
 }
 
-console.log(ulrExist());
-console.log("AAAAAA")
+// Listen for changes in the OS settings.
+// Note: the arrow function shorthand works only in modern browsers,
+// for older browsers define the function using the function keyword.
+useDark.addListener((evt) => toggleDarkMode(evt.matches));
+
+// Toggles the "dark-mode" class on click and sets localStorage state
+button.addEventListener("click", () => {
+  darkModeState = !darkModeState;
+
+  toggleDarkMode(darkModeState);
+  setDarkModeLocalStorage(darkModeState);
+});
+
+function tdnn() {
+  document.getElementsByClassName("moon")[0].classList.toggle("sun");
+  document.getElementsByClassName("tdnn")[0].classList.toggle("day");
+  document.getElementsByTagName("BODY")[0].classList.toggle("light");
+}
 
 
-// function hasLogIn(){
-//     if (window.localStorage.getItem("currentUser") in ["", null, undefined]){
-//         return false;
-//     }
-//     else{return true}
-// }
-// if(hasLogIn()){
-//     accountName = window.localStorage.getItem("currentUser");
-//     accountLabel = document.getElementById("account").innerHTML = accountName
-// }
 
-window.onload = function(){
-    // darkmode toggle
-    let darkModeState = false;
+var currentUser = window.localStorage.getItem("currentUser");
+function hasLogIn() {
+  if (window.localStorage.getItem("currentUser") == "null") {
+    return false;
+  }
+  else { return true }
+}
 
-    const button = document.querySelector("#dark-mode-toggle");
 
-    // MediaQueryList object
-    const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+if (hasLogIn()) {
+  console.log(window.localStorage.getItem("currentUser"));
+  document.getElementById("login-element").innerHTML = '<img src="img/account-photo-profile.jpg" alt"" class="account-photo-profile">' + currentUser;
+  accountName = window.localStorage.getItem("currentUser");
+}
+else {
+  currentUser = window.localStorage.getItem("currentUser");
+}
 
-    // Toggles the "dark-mode" class
-    function toggleDarkMode(state) {
-        document.documentElement.classList.toggle("dark-mode", state);
-        darkModeState = state;
+// logout
+const accountElement = document.getElementById("login-element");
+
+accountElement.addEventListener("click", function () {
+  if (hasLogIn()) {
+    if (confirm("Logout dari akun?") == true) {
+    window.localStorage.setItem("currentUser", "null");
+    window.location = "home.html";
+    } else {
+
     }
+  }
+});
 
-    // Sets localStorage state
-    function setDarkModeLocalStorage(state) {
-        localStorage.setItem("dark-mode", state);
-    }
 
-    // Initial setting
-    toggleDarkMode(localStorage.getItem("dark-mode") == "true");
-
-    // Listen for changes in the OS settings.
-    // Note: the arrow function shorthand works only in modern browsers,
-    // for older browsers define the function using the function keyword.
-    useDark.addListener((evt) => toggleDarkMode(evt.matches));
-
-    // Toggles the "dark-mode" class on click and sets localStorage state
-    button.addEventListener("click", () => {
-        darkModeState = !darkModeState;
-
-        toggleDarkMode(darkModeState);
-        setDarkModeLocalStorage(darkModeState);
-    });
-}
